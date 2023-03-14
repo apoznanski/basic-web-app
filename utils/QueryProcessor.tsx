aeec85ba3e6cb539de0c787aec881039ac937fdf
apoznanski <71227648+apoznanski@users.noreply.github.com>
2023-03-14 16:11:17 -0400

    actual query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -11,38 +11,16 @@ export default function QueryProcessor(query: string): string {
       "David M. and Adriana P."
     );
   }
-  if (query.toLowerCase().includes("what is 17 + 43?")) {
-    return (
-      "60"
-    );
-  }
-  if (query.toLowerCase().includes("what is 66 + 73?")) {
-    return (
-      "139"
-    );
-  }
-  if (query.toLowerCase().includes("what is 50 + 48?")) {
-    return (
-      "98"
-    );
-  }  
-  if (query.toLowerCase().includes("which of the following numbers is the largest: 46, 48, 84?"
-    )) {
-    return (
-      "84"
-    );
-  }
-  if (query.toLowerCase().includes("which of the following numbers is the largest: 95, 56, 45?"
-  )) {
-  return (
-    "95"
-  );
-  }
-  if (query.toLowerCase().includes("which of the following numbers is the largest: 64, 85, 47?"
-  )) {
-  return (
-    "85"
-  );
+  if (query.toLowerCase().includes("what is + ?")) {
+    const regex = /(-?\d+(\.\d+)?)\s*\+\s*(-?\d+(\.\d+)?)/; // matches "x + y" pattern
+    const match = regex.exec(query);
+
+    if (match) {
+      const x = parseFloat(match[1]); // extract x as float from regex match
+      const y = parseFloat(match[3]); // extract y as float from regex match
+      const sum = x + y;
+      return "${sum}";
+    }
   }
   if (query.toLowerCase().includes("which of the following numbers is the largest: 64, 20, 78?"
   )) {
@@ -50,148 +28,5 @@ export default function QueryProcessor(query: string): string {
     "78"
   );
   }
-  if (query.toLowerCase().includes("what is 91 + 65?")) {
-    return (
-      "156"
-    );
-  }  
-  if (query.toLowerCase().includes("what is 69 + 88?")) {
-    return (
-      "157"
-    );
-  }  
-  if (query.toLowerCase().includes("what is 52 + 99?")) {
-    return (
-      "151"
-    );
-  }
-  if (query.toLowerCase().includes("what is 75 + 20?")) {
-    return (
-      "95"
-    );
-  }  
-  if (query.toLowerCase().includes("what is 52 + 76?")) {
-    return (
-      "128"
-    );
-  }  
-  if (query.toLowerCase().includes("what is 87 + 76?")) {
-    return (
-      "163"
-    );
-  }  
-  if (query.toLowerCase().includes("which of the following numbers is the largest: 90, 18, 88?"
-    )) {
-    return (
-      "90"
-    );
-  }
-  if (query.toLowerCase().includes("which of the following numbers is the largest: 14, 28, 52?"
-    )) {
-    return (
-      "52"
-    );
-  }
-  if (query.toLowerCase().includes("what is 22 + 66?")) {
-    return (
-      "88"
-    );
-  }  
-  if (query.toLowerCase().includes("which of the following numbers is the largest: 18, 49, 40?"
-  )) {
-  return (
-    "49"
-  );
-  }
-  if (query.toLowerCase().includes("which of the following numbers is the largest: 53, 67, 49?"
-  )) {
-  return (
-    "67"
-  );
-  if (query.toLowerCase().includes("which of the following numbers is the largest: 30, 22, 25?"
-  )) {
-  return (
-    "30"
-  );
-  }
-  }
-  if (query.toLowerCase().includes("what is 89 + 28?")) {
-    return (
-      "117"
-    );
-  }  
-  if (query.toLowerCase().includes("which of the following numbers is the largest: 65, 42, 21?"
-  )) {
-  return (
-    "65"
-  );
-  }
-  if (query.toLowerCase().includes("what is 92 + 43?")) {
-    return (
-      "145"
-    );
-  }  
-  if (query.toLowerCase().includes("which of the following numbers is the largest: 36, 12, 31?"
-  )) {
-  return (
-    "36"
-  );
-  }
-  if (query.toLowerCase().includes("which of the following numbers is the largest: 62, 20, 53?"
-  )) {
-  return (
-    "62"
-  );
-  }
-  if (query.toLowerCase().includes("which of the following numbers is the largest: 76, 33, 28?"
-  )) {
-  return (
-    "76"
-  );
-  }
-  if (query.toLowerCase().includes("which of the following numbers is the largest: 28, 36, 64?"
-  )) {
-  return (
-    "64"
-  );
-  }
-  if (query.toLowerCase().includes("which of the following numbers is the largest: 30, 6, 1?"
-  )) {
-  return (
-    "30"
-  );
-  }
-  if (query.toLowerCase().includes("which of the following numbers is the largest: 80, 35, 17?"
-  )) {
-  return (
-    "80"
-  );
-  }
-  if (query.toLowerCase().includes("which of the following numbers is the largest: 62, 37, 44?"
-  )) {
-  return (
-    "62"
-  );
-  }
-  if (query.toLowerCase().includes("what is 58 + 20?")) {
-    return (
-      "78"
-    );
-  }  
-  if (query.toLowerCase().includes("what is 25 + 46?")) {
-    return (
-      "71"
-    );
-  }  
-  if (query.toLowerCase().includes("what is 53 + 52?")) {
-    return (
-      "105"
-    );
-  } 
-  if (query.toLowerCase().includes("what is 49 + 78?")) {
-    return (
-      "127"
-    );
-  }   
   return "";
 }
